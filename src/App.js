8c8107e3ce61c7ae3f260bcae792d0c182ebf314
Vedat Özayar <vedatozayar@gmail.com>
2018-09-18 22:49:18 +0300

    ikinci commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Welcome to vedat</h1>
         </header>
         <p className="App-intro">
           To get started, editasdfasdfasdfasdf <code>src/App.js</code> and save to reload.
